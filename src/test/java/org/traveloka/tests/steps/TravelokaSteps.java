45487d19adb89bcfb102b8e3e827954318ceb2f9
edgrmaulana <edgrmaulana@gmail.com>
2024-02-12 20:23:26 +0700

    creating framework for traveloka automation tests

--- src/test/java/org/traveloka/tests/steps/TravelokaSteps.java
+++ src/test/java/org/traveloka/tests/steps/TravelokaSteps.java
@@ -2,7 +2,6 @@ package org.traveloka.tests.steps;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.traveloka.tests.properties.TravelokaProperties;
 import org.traveloka.tests.utils.CommonUtility;
 
@@ -10,14 +9,9 @@ import java.io.IOException;
 
 public class TravelokaSteps {
 
-//    @Autowired
-//    CommonUtility commonUtility;
-
     CommonUtility commonUtility = new CommonUtility();
 
     TravelokaProperties travelokaProperties = new TravelokaProperties();
-//    @Autowired
-//    TravelokaProperties travelokaProperties;
 
     @Given("[travelokapage] User tried to open traveloka page")
     public void travelokapageUserTriedToOpenTravelokaPage() throws IOException {
